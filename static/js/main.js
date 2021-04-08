1400f578f8ae1ed97d75f1477476dea386a178a5
wilko90 <mark_wilkinson2010@hotmail.co.uk>
2021-04-08 15:40:26 +0000

    feat:add scroll reveal added to home,inspiration,about us and contact

---
+++ static/js/main.js
@@ -0,0 +1,10 @@
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 2200,
+    reset: true
+});
+
+sr.reveal(`.home-margin, .initial-home-image, .veg-image-size, .about-us-col, .inspiration-reveal, .card-1-reveal, .card-2-reveal, .card-3-reveal, .contact-reveal, .contact-button`, {
+    interval: 200
+})
