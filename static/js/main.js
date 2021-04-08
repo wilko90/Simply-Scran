3b7ab5349ea413afa3f7ea521e10efc8856f3c80
wilko90 <mark_wilkinson2010@hotmail.co.uk>
2021-04-08 16:33:35 +0000

    feat: add 3 more inspiration cards

--- static/js/main.js
+++ static/js/main.js
@@ -5,6 +5,6 @@ const sr = ScrollReveal({
     reset: true
 });
 
-sr.reveal(`.home-margin, .initial-home-image, .veg-image-size, .about-us-col, .inspiration-reveal, .card-1-reveal, .card-2-reveal, .card-3-reveal, .contact-reveal, .contact-button`, {
+sr.reveal(`.home-margin, .initial-home-image, .veg-image-size, .about-us-col, .inspiration-reveal, .card-1-reveal, .card-2-reveal, .card-3-reveal, .card-4-reveal, .card-5-reveal, .card-6-reveal, .contact-reveal, .contact-button`, {
     interval: 200
 })
