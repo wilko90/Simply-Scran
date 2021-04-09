9a219c42f6b828724fbb8d53dfcdd72f74d6664e
wilko90 <mark_wilkinson2010@hotmail.co.uk>
2021-04-09 13:17:22 +0000

    feat: add spoonacular api to the console

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,20 @@
+fetch("https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/324694/analyzedInstructions?stepBreakdown=true", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-key": "ae23dff16dmsh22f76a1569fe669p1624f8jsn623c761b8b11",
+		"x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com"
+	}
+})
+.then(response => response.json())
+.then(response => {
+	console.log(response);
+})
+.catch(err => {
+	console.error(err);
+});
+
+
+
 const sr = ScrollReveal({
     origin: 'top',
     distance: '30px',
@@ -8,3 +25,6 @@ const sr = ScrollReveal({
 sr.reveal(`.home-margin, .initial-home-image, .veg-image-size, .about-us-col, .inspiration-reveal, .card-1-reveal, .card-2-reveal, .card-3-reveal, .card-4-reveal, .card-5-reveal, .card-6-reveal, .contact-reveal, .contact-button`, {
     interval: 200
 })
+
+
+$("#exampleModal").modal("hide");
