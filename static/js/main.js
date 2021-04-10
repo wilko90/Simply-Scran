e4304e1d3fda60d59e1caec37a9998a6f17a78ac
wilko90 <mark_wilkinson2010@hotmail.co.uk>
2021-04-10 17:37:34 +0000

    feat: api rendered to card images

--- static/js/main.js
+++ static/js/main.js
@@ -2,24 +2,47 @@ const searchForm = document.querySelector('form');
 const searchResultDiv = document.querySelector('.search-data');
 const container = document.querySelector('.container');
 let searchQuery = '';
-const APP_Key = 'ae23dff16dmsh22f76a1569fe669p1624f8jsn623c761b8b11';
-const APP_Host = "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com"
-
-
+const APP_ID = '8cd9dfe4';
+const APP_key = 'd4c908c465e03754de3addc77af6a8a7'
+const baseURL = `https://api.edamam.com/search?q=${searchQuery}&app_id=${APP_ID}&app_key=${APP_key}`;
 
 searchForm.addEventListener('submit', (e) => {
     e.preventDefault();
     searchQuery = e.target.querySelector('input').value;
     fetchAPI();
 
-});
+})
 
 async function fetchAPI (){
-    
-    const baseURL = `https://api.spoonacular.com/recipes/complexSearch?q=${searchQuery}&app_key=${APP_Key}&app_host=${APP_Host}&to=6`;
-    const response = await fetch(baseURL);
-    const data = await response.json()
+    const baseURL = `https://api.edamam.com/search?q=${searchQuery}&app_id=${APP_ID}&app_key=${APP_key}&to=6`;
+    const response = await fetch (baseURL)
+    const data = await response.json();
+    generateHTML(data.hits);
     console.log(data)
+}
+function generateHTML(results){
+    let generatedHTML = '';
+    results.map(result => {
+      generatedHTML +=
+     
+      `
+      <div class="card mb-4 box-shadow">
+                    <img class="card-img-top" src="${result.recipe.image}">
+                    <div class="card-body">
+                        <p class="card-text">${result.recipe.label}</p>
+                        <div class="d-flex justify-content-between align-items-center">
+                            <div class="btn-group">
+                                <button type="button" class="btn btn-sm btn-outline-secondary" data-toggle="modal"
+                                    data-target="#exampleModal">View Recipe</button>
+
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+                `
+    })
+    searchResultDiv.innerHTML = generatedHTML;
 }
 
 "/======SCROLL REVEAL======/"
@@ -32,7 +55,6 @@ const sr = ScrollReveal({
 
 sr.reveal(`.home-margin, .initial-home-image, .veg-image-size, .about-us-col, .inspiration-reveal, .card-1-reveal, .card-2-reveal, .card-3-reveal, .card-4-reveal, .card-5-reveal, .card-6-reveal, .contact-reveal, .contact-button`, {
     interval: 200
-})
+});
 
 
-$("#exampleModal").modal("hide");
