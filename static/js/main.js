9176b2c96ba8bb0eb40bda8456b46c85a621a506
wilko90 <mark_wilkinson2010@hotmail.co.uk>
2021-04-12 14:40:30 +0000

    feat:add google api,remove recipe api

--- static/js/main.js
+++ static/js/main.js
@@ -1,49 +1,8 @@
-const searchForm = document.querySelector('form');
-const searchResultDiv = document.querySelector('.search-data');
-const container = document.querySelector('.container');
-let searchQuery = '';
-const APP_ID = '8cd9dfe4';
-const APP_key = 'd4c908c465e03754de3addc77af6a8a7'
-const baseURL = `https://api.edamam.com/search?q=${searchQuery}&app_id=${APP_ID}&app_key=${APP_key}`;
 
-searchForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    searchQuery = e.target.querySelector('input').value;
-    fetchAPI();
 
-})
 
-async function fetchAPI (){
-    const baseURL = `https://api.edamam.com/search?q=${searchQuery}&app_id=${APP_ID}&app_key=${APP_key}&to=6`;
-    const response = await fetch (baseURL)
-    const data = await response.json();
-    generateHTML(data.hits);
-    console.log(data)
-}
-function generateHTML(results){
-    let generatedHTML = '';
-    results.map(result => {
-      generatedHTML +=
-     
-      `
-      <div class="card mb-4 box-shadow">
-                    <img class="card-img-top" src="${result.recipe.image}">
-                    <div class="card-body">
-                        <p class="card-text">${result.recipe.label}</p>
-                        <div class="d-flex justify-content-between align-items-center">
-                            <div class="btn-group">
-                                <button type="button" class="btn btn-sm btn-outline-secondary" data-toggle="modal"
-                                    data-target="#exampleModal">View Recipe</button>
 
-                            </div>
 
-                        </div>
-                    </div>
-                </div>
-                `
-    })
-    searchResultDiv.innerHTML = generatedHTML;
-}
 
 "/======SCROLL REVEAL======/"
 const sr = ScrollReveal({
---
+++ static/js/map.js
@@ -0,0 +1,12 @@
+
+function initMap(){
+    
+    var options = {
+    center: { lat: 38.3460, lng:-0.4907 },
+    zoom: 8
+  
+};
+
+
+  map = new google.maps.Map(document.getElementById("map"),options)
+}
