e2a3845642547699e19f477c80f82d790a73dee9
wilko90 <mark_wilkinson2010@hotmail.co.uk>
2021-04-12 19:01:02 +0000

    feat: add geo location navigator to maps

--- static/js/map.js
+++ static/js/map.js
@@ -47,6 +47,32 @@ function createMap() {
 
         map.fitBounds(bounds);
     });
+    map = new google.maps.Map(document.getElementById('map'), options);
+  infoWindow = new google.maps.InfoWindow;
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function (p) {
+      var position = {
+        lat: p.coords.latitude,
+        lng: p.coords.longitude
+      };
+
+      infoWindow.setPosition(position);
+      infoWindow.setContent('Your location!');
+      infoWindow.open(map);
+      map.setCenter(position);
+    }, function () {
+      handleLocationError('Geolocation service failed', map.getCenter());
+    });
+  } else {
+    handleLocationError('No geolocation available.', map.getCenter());
+  }
 }
 
-//====GEO LOCATION SERVICES====//
+function handleLocationError (content, position) {
+  infoWindow.setPosition(position);
+  infoWindow.setContent(content);
+  infoWindow.open(map);
+}
+
+
