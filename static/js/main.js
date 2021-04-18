dc8f7d0e73b3dffe48e028905bfa98b7be205cf8
wilko90 <mark_wilkinson2010@hotmail.co.uk>
2021-04-18 11:15:11 +0000

    feat: add scroll section js and styled

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,25 @@
 "/======SCROLL SECTIONS======/"
+const sections = document.querySelectorAll('section');
+const navLi = document.querySelectorAll('nav .container ul li');
 
+window.addEventListener('scroll', ()=> {
+    let current = '';
+
+    sections.forEach( section =>{
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.clientHeight;
+        if(pageYOffset >= (sectionTop - sectionHeight / 3)){
+            current = section.getAttribute('id');
+        }
+    })
+    
+    navLi.forEach( li => {
+        li.classList.remove('active-nav');
+        if(li.classList.contains(current)){
+            li.classList.add('active-nav')
+        }
+    })
+})
 
 
 
